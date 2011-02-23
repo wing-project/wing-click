3a42eef19d4c0ff0b755061a8090c48925a79380
rriggio <rriggio@cn-rriggio.(none)>
2011-02-23 15:17:32 +0100

    add full fledged radiotap parser

--- elements/wifi/radiotapdecap.cc
+++ elements/wifi/radiotapdecap.cc
@@ -21,107 +21,51 @@
 #include <click/confparse.hh>
 #include <click/error.hh>
 #include <click/glue.hh>
+#include <click/radiotap_iter.h>
+#include <click/packet_anno.hh>
 #include <clicknet/wifi.h>
 #include <clicknet/radiotap.h>
-#include <click/packet_anno.hh>
 #include <clicknet/llc.h>
 CLICK_DECLS
 
-#define NUM_RADIOTAP_ELEMENTS 18
-
-static const int radiotap_elem_to_bytes[NUM_RADIOTAP_ELEMENTS] =
-	{8, /* IEEE80211_RADIOTAP_TSFT */
-	 1, /* IEEE80211_RADIOTAP_FLAGS */
-	 1, /* IEEE80211_RADIOTAP_RATE */
-	 4, /* IEEE80211_RADIOTAP_CHANNEL */
-	 2, /* IEEE80211_RADIOTAP_FHSS */
-	 1, /* IEEE80211_RADIOTAP_DBM_ANTSIGNAL */
-	 1, /* IEEE80211_RADIOTAP_DBM_ANTNOISE */
-	 2, /* IEEE80211_RADIOTAP_LOCK_QUALITY */
-	 2, /* IEEE80211_RADIOTAP_TX_ATTENUATION */
-	 2, /* IEEE80211_RADIOTAP_DB_TX_ATTENUATION */
-	 1, /* IEEE80211_RADIOTAP_DBM_TX_POWER */
-	 1, /* IEEE80211_RADIOTAP_ANTENNA */
-	 1, /* IEEE80211_RADIOTAP_DB_ANTSIGNAL */
-	 1, /* IEEE80211_RADIOTAP_DB_ANTNOISE */
-	 2, /* IEEE80211_RADIOTAP_RX_FLAGS */
-	 2, /* IEEE80211_RADIOTAP_TX_FLAGS */
-	 1, /* IEEE80211_RADIOTAP_RTS_RETRIES */
-	 1, /* IEEE80211_RADIOTAP_DATA_RETRIES */
-	};
-
-static const int radiotap_required_align[NUM_RADIOTAP_ELEMENTS] =
-	{4, /* IEEE80211_RADIOTAP_TSFT */
-	 1, /* IEEE80211_RADIOTAP_FLAGS */
-	 1, /* IEEE80211_RADIOTAP_RATE */
-	 2, /* IEEE80211_RADIOTAP_CHANNEL */
-	 1, /* IEEE80211_RADIOTAP_FHSS */
-	 1, /* IEEE80211_RADIOTAP_DBM_ANTSIGNAL */
-	 1, /* IEEE80211_RADIOTAP_DBM_ANTNOISE */
-	 2, /* IEEE80211_RADIOTAP_LOCK_QUALITY */
-	 2, /* IEEE80211_RADIOTAP_TX_ATTENUATION */
-	 2, /* IEEE80211_RADIOTAP_DB_TX_ATTENUATION */
-	 1, /* IEEE80211_RADIOTAP_DBM_TX_POWER */
-	 1, /* IEEE80211_RADIOTAP_ANTENNA */
-	 1, /* IEEE80211_RADIOTAP_DB_ANTSIGNAL */
-	 1, /* IEEE80211_RADIOTAP_DB_ANTNOISE */
-	 2, /* IEEE80211_RADIOTAP_RX_FLAGS */
-	 2, /* IEEE80211_RADIOTAP_TX_FLAGS */
-	 1, /* IEEE80211_RADIOTAP_RTS_RETRIES */
-	 1, /* IEEE80211_RADIOTAP_DATA_RETRIES */
-	};
-
-
-static int rt_el_present(struct ieee80211_radiotap_header *th, u_int32_t element)
+static void print_radiotap_namespace(struct ieee80211_radiotap_iterator *iter)
 {
-	if (element > NUM_RADIOTAP_ELEMENTS)
-		return 0;
-	return le32_to_cpu(th->it_present) & (1 << element);
-}
-
-static int rt_check_header(struct ieee80211_radiotap_header *th, int len)
-{
-	int bytes = 0;
-	int x = 0;
-	if (th->it_version != 0) {
-		return 0;
+	switch (iter->this_arg_index) {
+	case IEEE80211_RADIOTAP_TSFT:
+		printf("\tTSFT: %llu\n", le64toh(*(unsigned long long *)iter->this_arg));
+		break;
+	case IEEE80211_RADIOTAP_FLAGS:
+		printf("\tflags: %02x\n", *iter->this_arg);
+		break;
+	case IEEE80211_RADIOTAP_RATE:
+		printf("\trate: %lf\n", (double)*iter->this_arg/2);
+		break;
+	case IEEE80211_RADIOTAP_CHANNEL:
+	case IEEE80211_RADIOTAP_FHSS:
+	case IEEE80211_RADIOTAP_DBM_ANTSIGNAL:
+	case IEEE80211_RADIOTAP_DBM_ANTNOISE:
+	case IEEE80211_RADIOTAP_LOCK_QUALITY:
+	case IEEE80211_RADIOTAP_TX_ATTENUATION:
+	case IEEE80211_RADIOTAP_DB_TX_ATTENUATION:
+	case IEEE80211_RADIOTAP_DBM_TX_POWER:
+	case IEEE80211_RADIOTAP_ANTENNA:
+	case IEEE80211_RADIOTAP_DB_ANTSIGNAL:
+	case IEEE80211_RADIOTAP_DB_ANTNOISE:
+	case IEEE80211_RADIOTAP_TX_FLAGS:
+		break;
+	case IEEE80211_RADIOTAP_RX_FLAGS:
+		printf("\tRX flags: %#.4x\n",
+			le16toh(*(uint16_t *)iter->this_arg));
+		break;
+	case IEEE80211_RADIOTAP_RTS_RETRIES:
+		break;
+	case IEEE80211_RADIOTAP_DATA_RETRIES:
+		printf("\tretries: %d\n", *iter->this_arg);
+		break;
+	default:
+		printf("\tBOGUS DATA\n");
+		break;
 	}
-
-	if (le16_to_cpu(th->it_len) < sizeof(struct ieee80211_radiotap_header)) {
-		return 0;
-	}
-
-	for (x = 0; x < NUM_RADIOTAP_ELEMENTS; x++) {
-		if (rt_el_present(th, x))
-		    bytes += radiotap_elem_to_bytes[x];
-	}
-
-	if (le16_to_cpu(th->it_len) < sizeof(struct ieee80211_radiotap_header) + bytes) {
-		return 0;
-	}
-
-	if (le16_to_cpu(th->it_len) > len) {
-		return 0;
-	}
-
-	return 1;
-}
-
-static u_int8_t *rt_el_offset(struct ieee80211_radiotap_header *th, u_int32_t element) {
-	unsigned int x = 0;
-	u_int8_t *offset = ((u_int8_t *) th) + sizeof(ieee80211_radiotap_header);
-	u_int8_t bound = 0;
-	for (x = 0; x < NUM_RADIOTAP_ELEMENTS && x < element; x++) {
-		if (rt_el_present(th, x)) {
-			u_int8_t req = radiotap_required_align[x];
-			u_int8_t push = radiotap_elem_to_bytes[x] + (req - (bound % req)) % req;
-			offset += push;
-			bound = (bound + push) % 4;
-		}
-	}
-	u_int8_t req = radiotap_required_align[element];
-	offset += (req - (bound % req)) % req;
-	return offset;
 }
 
 RadiotapDecap::RadiotapDecap()
@@ -135,114 +79,140 @@ RadiotapDecap::~RadiotapDecap()
 int
 RadiotapDecap::configure(Vector<String> &conf, ErrorHandler *errh)
 {
-
-  _debug = false;
-  if (cp_va_kparse(conf, this, errh,
-		   "DEBUG", 0, cpBool, &_debug,
-		   cpEnd) < 0)
-    return -1;
-  return 0;
+	_debug = false;
+	if (cp_va_kparse(conf, this, errh,
+			"DEBUG", 0, cpBool, &_debug,
+			cpEnd) < 0)
+		return -1;
+	return 0;
 }
 
 Packet *
-RadiotapDecap::simple_action(Packet *p)
-{
-	struct ieee80211_radiotap_header *th = (struct ieee80211_radiotap_header *) p->data();
-	struct click_wifi_extra *ceh = WIFI_EXTRA_ANNO(p);
-	if (rt_check_header(th, p->length())) {
-		memset((void*)ceh, 0, sizeof(struct click_wifi_extra));
-		ceh->magic = WIFI_EXTRA_MAGIC;
+RadiotapDecap::simple_action(Packet *p) {
 
-		if (rt_el_present(th, IEEE80211_RADIOTAP_FLAGS)) {
-			u_int8_t flags = *((u_int8_t *) rt_el_offset(th, IEEE80211_RADIOTAP_FLAGS));
+	struct ieee80211_radiotap_header *th = (struct ieee80211_radiotap_header *) p->data();
+	struct ieee80211_radiotap_iterator iter;
+	struct click_wifi_extra *ceh = WIFI_EXTRA_ANNO(p);
+
+	int err = ieee80211_radiotap_iterator_init(&iter, th, p->length(), 0);
+
+	if (err) {
+		click_chatter("%{element} :: %s :: malformed radiotap header (init returns %d)", this, __func__, err);
+		goto drop;
+	}
+
+	memset((void*)ceh, 0, sizeof(struct click_wifi_extra));
+	ceh->magic = WIFI_EXTRA_MAGIC;
+
+	while (!(err = ieee80211_radiotap_iterator_next(&iter))) {
+		print_radiotap_namespace(&iter);
+		u_int16_t flags;
+		switch (iter.this_arg_index) {
+		case IEEE80211_RADIOTAP_FLAGS:
+			flags = le16_to_cpu(*iter.this_arg);
 			if (flags & IEEE80211_RADIOTAP_F_DATAPAD) {
 				ceh->pad = 1;
 			}
 			if (flags & IEEE80211_RADIOTAP_F_FCS) {
 				p->take(4);
 			}
-		}
-
-		if (rt_el_present(th, IEEE80211_RADIOTAP_RATE)) {
-			ceh->rate = *((u_int8_t *) rt_el_offset(th, IEEE80211_RADIOTAP_RATE));
-		}
-
-		if (rt_el_present(th, IEEE80211_RADIOTAP_DBM_ANTSIGNAL))
-			ceh->rssi = *((u_int8_t *) rt_el_offset(th, IEEE80211_RADIOTAP_DBM_ANTSIGNAL));
-
-		if (rt_el_present(th, IEEE80211_RADIOTAP_CHANNEL)) 
-			ceh->channel = le16_to_cpu(*((u_int16_t *) rt_el_offset(th, IEEE80211_RADIOTAP_CHANNEL)));
-
-		if (rt_el_present(th, IEEE80211_RADIOTAP_DBM_ANTNOISE))
-			ceh->silence = *((u_int8_t *) rt_el_offset(th, IEEE80211_RADIOTAP_DBM_ANTNOISE));
-
-		if (rt_el_present(th, IEEE80211_RADIOTAP_DB_ANTSIGNAL))
-			ceh->rssi = *((u_int8_t *) rt_el_offset(th, IEEE80211_RADIOTAP_DB_ANTSIGNAL));
-
-		if (rt_el_present(th, IEEE80211_RADIOTAP_DB_ANTNOISE))
-			ceh->silence = *((u_int8_t *) rt_el_offset(th, IEEE80211_RADIOTAP_DB_ANTNOISE));
-
-		if (rt_el_present(th, IEEE80211_RADIOTAP_RX_FLAGS)) {
-			u_int16_t flags = le16_to_cpu(*((u_int16_t *) rt_el_offset(th, IEEE80211_RADIOTAP_RX_FLAGS)));
-			if (flags & IEEE80211_RADIOTAP_F_RX_BADFCS)
+			break;
+		case IEEE80211_RADIOTAP_RATE:
+			ceh->rate = *iter.this_arg;
+			break;
+		case IEEE80211_RADIOTAP_DATA_RETRIES:
+			ceh->retries = *iter.this_arg;
+			break;
+		case IEEE80211_RADIOTAP_CHANNEL:
+			ceh->channel = le16_to_cpu(*iter.this_arg);
+			break;
+		case IEEE80211_RADIOTAP_DBM_ANTSIGNAL:
+			ceh->rssi = *iter.this_arg;
+			break;
+		case IEEE80211_RADIOTAP_DBM_ANTNOISE:
+			ceh->silence = *iter.this_arg;
+			break;
+		case IEEE80211_RADIOTAP_DB_ANTSIGNAL:
+			ceh->rssi = *iter.this_arg;
+			break;
+		case IEEE80211_RADIOTAP_DB_ANTNOISE:
+			ceh->silence = *iter.this_arg;
+			break;
+		case IEEE80211_RADIOTAP_RX_FLAGS:
+			flags = le16_to_cpu(*iter.this_arg);
+			if (flags & IEEE80211_RADIOTAP_F_BADFCS)
 				ceh->flags |= WIFI_EXTRA_RX_ERR;
-		}
-
-		if (rt_el_present(th, IEEE80211_RADIOTAP_TX_FLAGS)) {
-			u_int16_t flags = le16_to_cpu(*((u_int16_t *) rt_el_offset(th, IEEE80211_RADIOTAP_TX_FLAGS)));
+			break;
+		case IEEE80211_RADIOTAP_TX_FLAGS:
+			flags = le16_to_cpu(*iter.this_arg);
 			ceh->flags |= WIFI_EXTRA_TX;
 			if (flags & IEEE80211_RADIOTAP_F_TX_FAIL)
 				ceh->flags |= WIFI_EXTRA_TX_FAIL;
+			break;
+
+		default:
+			click_chatter("%{element} :: %s :: unsupported field %d", 
+					this, 
+					__func__, 
+					iter.this_arg_index);
+			break;
 		}
-
-		if (rt_el_present(th, IEEE80211_RADIOTAP_DATA_RETRIES))
-			ceh->retries = *((u_int8_t *) rt_el_offset(th, IEEE80211_RADIOTAP_DATA_RETRIES));
-
-		p->pull(le16_to_cpu(th->it_len));
-		p->set_mac_header(p->data());  // reset mac-header pointer
 	}
 
-  return p;
-}
+	if (err != -ENOENT) {
+		click_chatter("%{element} :: %s :: malformed radiotap data", this, __func__);
+		goto drop;
+	}
 
+	p->pull(le16_to_cpu(th->it_len));
+	p->set_mac_header(p->data());  // reset mac-header pointer
+
+	return p;
+
+  drop:
+
+	p->kill();
+	return 0;
+
+}
 
 enum {H_DEBUG};
 
 static String
 RadiotapDecap_read_param(Element *e, void *thunk)
 {
-  RadiotapDecap *td = (RadiotapDecap *)e;
-    switch ((uintptr_t) thunk) {
-      case H_DEBUG:
-	return String(td->_debug) + "\n";
-    default:
-      return String();
-    }
+	RadiotapDecap *td = (RadiotapDecap *)e;
+	switch ((uintptr_t) thunk) {
+		case H_DEBUG:
+			return String(td->_debug) + "\n";
+		default:
+			return String();
+	}
 }
+
 static int
-RadiotapDecap_write_param(const String &in_s, Element *e, void *vparam,
-		      ErrorHandler *errh)
+RadiotapDecap_write_param(const String &in_s, Element *e, void *vparam, ErrorHandler *errh)
 {
-  RadiotapDecap *f = (RadiotapDecap *)e;
-  String s = cp_uncomment(in_s);
-  switch((intptr_t)vparam) {
-  case H_DEBUG: {    //debug
-    bool debug;
-    if (!cp_bool(s, &debug))
-      return errh->error("debug parameter must be boolean");
-    f->_debug = debug;
-    break;
-  }
-  }
-  return 0;
+	RadiotapDecap *f = (RadiotapDecap *)e;
+	String s = cp_uncomment(in_s);
+	switch((intptr_t)vparam) {
+		case H_DEBUG: {
+			bool debug;
+			if (!cp_bool(s, &debug))
+				return errh->error("debug parameter must be boolean");
+			f->_debug = debug;
+			break;
+		}
+	}
+	return 0;
 }
 
 void
 RadiotapDecap::add_handlers()
 {
-  add_read_handler("debug", RadiotapDecap_read_param, (void *) H_DEBUG);
-
-  add_write_handler("debug", RadiotapDecap_write_param, (void *) H_DEBUG);
+	add_read_handler("debug", RadiotapDecap_read_param, (void *) H_DEBUG);
+	add_write_handler("debug", RadiotapDecap_write_param, (void *) H_DEBUG);
 }
+
 CLICK_ENDDECLS
 EXPORT_ELEMENT(RadiotapDecap)
